4707ffbc356059c60ca6c5653a42e25f57ed4394
Abram <abramwaldner4@gmail.com>
2025-11-07 12:32:55 -0600

    license page

--- server/middleware/licenseAuth.js
+++ server/middleware/licenseAuth.js
@@ -16,16 +16,29 @@ module.exports = async function licenseAuth(req, res, next) {
     const key = (req.cookies && req.cookies.licenseKey) || req.get('x-license-key') || req.query.license;
     if (!key) {
       console.debug('[licenseAuth] missing license key for', req.path);
+      // If this is a browser HTML GET, redirect to license entry page with original URL so user can come back after entering key.
+      if (req.method === 'GET' && req.accepts && req.accepts('html')) {
+        const redirectTo = encodeURIComponent(req.originalUrl || req.url || req.path);
+        return res.redirect('/license.html?redirect=' + redirectTo);
+      }
       return res.status(403).json({ error: 'license required' });
     }
 
     const license = await License.findOne({ key });
     if (!license) {
       console.debug('[licenseAuth] invalid license key for', req.path);
+      if (req.method === 'GET' && req.accepts && req.accepts('html')) {
+        const redirectTo = encodeURIComponent(req.originalUrl || req.url || req.path);
+        return res.redirect('/license.html?redirect=' + redirectTo);
+      }
       return res.status(403).json({ error: 'invalid license' });
     }
     if (license.isExpired()) {
       console.debug('[licenseAuth] expired license', license.key);
+      if (req.method === 'GET' && req.accepts && req.accepts('html')) {
+        const redirectTo = encodeURIComponent(req.originalUrl || req.url || req.path);
+        return res.redirect('/license.html?redirect=' + redirectTo);
+      }
       return res.status(403).json({ error: 'license expired' });
     }
 
@@ -49,7 +62,7 @@ module.exports = async function licenseAuth(req, res, next) {
 module.exports.protectedStatic = function protectedStatic(staticMiddleware) {
   return function (req, res, next) {
     // Run licenseAuth; if it calls next() we continue to static middleware.
-    // If licenseAuth sends a response (403), staticMiddleware won't run.
+    // If licenseAuth sends a response (403 or redirect), staticMiddleware won't run.
     module.exports(req, res, function (err) {
       if (err) return next(err);
       staticMiddleware(req, res, next);
